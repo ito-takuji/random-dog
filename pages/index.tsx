3c7b0e38ec925f92e410b48c820f669eb9914c08
ito-takuji <ito-takuji@PC-0000706.local>
2023-05-12 17:13:37 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,51 @@
+import { GetServerSideProps, NextPage } from "next";
+import { useEffect, useState } from "react";
+
+type Props = {
+  initialImageUrl: string;
+};
+
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+    const [imageUrl, setImageUrl] = useState(initialImageUrl);
+    const [loading, setLoading] = useState(false);
+    // useEffect(() => {
+    //     fetchImage().then((newImage) => {
+    //         setImageUrl(newImage.url);
+    //         setLoading(false);
+    //     });
+    // }, []);
+  // ボタンをクリックしたときに画像を読み込む処理
+  const handleClick = async () => {
+    setLoading(true); // 読込中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>他のわんこも見る</button>
+      <div>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
+};
+export default IndexPage;
+
+export const GetServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+type Image = {
+  url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thedogapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+}
+
